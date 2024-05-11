258acf9c9a70ecc1b82046428eaecc2b084fa2e2
alexydens <alexydens@outlook.com>
2024-05-11 19:00:05 +0100

    Initial commit

---
+++ src/loadgl.h
@@ -0,0 +1,137 @@
+/* Include guard */
+#if !defined(LOADGL_H)
+#define LOADGL_H
+
+/* Includes */
+#include <nh_base.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <GL/glext.h>
+
+/* Buffers */
+PFNGLGENBUFFERSPROC glGenBuffers = NH_NULL;
+PFNGLBINDBUFFERPROC glBindBuffer = NH_NULL;
+PFNGLBUFFERDATAPROC glBufferData = NH_NULL;
+PFNGLDELETEBUFFERSPROC glDeleteBuffers = NH_NULL;
+/* Shaders */
+PFNGLCREATESHADERPROC glCreateShader = NH_NULL;
+PFNGLSHADERSOURCEPROC glShaderSource = NH_NULL;
+PFNGLCOMPILESHADERPROC glCompileShader = NH_NULL;
+PFNGLGETSHADERIVPROC glGetShaderiv = NH_NULL;
+PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog = NH_NULL;
+PFNGLDELETESHADERPROC glDeleteShader = NH_NULL;
+/* Programs */
+PFNGLCREATEPROGRAMPROC glCreateProgram = NH_NULL;
+PFNGLATTACHSHADERPROC glAttachShader = NH_NULL;
+PFNGLLINKPROGRAMPROC glLinkProgram = NH_NULL;
+PFNGLGETPROGRAMIVPROC glGetProgramiv = NH_NULL;
+PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog = NH_NULL;
+PFNGLUSEPROGRAMPROC glUseProgram = NH_NULL;
+PFNGLDELETEPROGRAMPROC glDeleteProgram = NH_NULL;
+/* Uniforms */
+PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = NH_NULL;
+PFNGLUNIFORM1FVPROC glUniform1fv = NH_NULL;
+PFNGLUNIFORM2FVPROC glUniform2fv = NH_NULL;
+PFNGLUNIFORM3FVPROC glUniform3fv = NH_NULL;
+PFNGLUNIFORM4FVPROC glUniform4fv = NH_NULL;
+PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv = NH_NULL;
+PFNGLUNIFORM1FPROC glUniform1f = NH_NULL;
+PFNGLUNIFORM1UIPROC glUniform1ui = NH_NULL;
+/* Attributes */
+PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation = NH_NULL;
+PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer = NH_NULL;
+PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray = NH_NULL;
+PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray = NH_NULL;
+PFNGLGENVERTEXARRAYSPROC glGenVertexArrays = NH_NULL;
+PFNGLBINDVERTEXARRAYPROC glBindVertexArray = NH_NULL;
+PFNGLDELETEVERTEXARRAYSPROC glDeleteVertexArrays = NH_NULL;
+/* Textures */
+PFNGLBINDIMAGETEXTUREPROC glBindImageTexture = NH_NULL;
+
+/* Compute shaders */
+PFNGLDISPATCHCOMPUTEPROC glDispatchCompute = NH_NULL;
+PFNGLMEMORYBARRIERPROC glMemoryBarrier = NH_NULL;
+
+/* Function to load */
+bool loadGL(void) {
+  /* Load OpenGL functions */
+
+  glGenBuffers = (PFNGLGENBUFFERSPROC) SDL_GL_GetProcAddress("glGenBuffers");
+  if (glGenBuffers == NH_NULL) return false;
+  glBindBuffer = (PFNGLBINDBUFFERPROC) SDL_GL_GetProcAddress("glBindBuffer");
+  if (glBindBuffer == NH_NULL) return false;
+  glBufferData = (PFNGLBUFFERDATAPROC) SDL_GL_GetProcAddress("glBufferData");
+  if (glBufferData == NH_NULL) return false;
+  glDeleteBuffers = (PFNGLDELETEBUFFERSPROC) SDL_GL_GetProcAddress("glDeleteBuffers");
+  if (glDeleteBuffers == NH_NULL) return false;
+
+  glCreateShader = (PFNGLCREATESHADERPROC) SDL_GL_GetProcAddress("glCreateShader");
+  if (glCreateShader == NH_NULL) return false;
+  glShaderSource = (PFNGLSHADERSOURCEPROC) SDL_GL_GetProcAddress("glShaderSource");
+  if (glShaderSource == NH_NULL) return false;
+  glCompileShader = (PFNGLCOMPILESHADERPROC) SDL_GL_GetProcAddress("glCompileShader");
+  if (glCompileShader == NH_NULL) return false;
+  glGetShaderiv = (PFNGLGETSHADERIVPROC) SDL_GL_GetProcAddress("glGetShaderiv");
+  if (glGetShaderiv == NH_NULL) return false;
+  glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC) SDL_GL_GetProcAddress("glGetShaderInfoLog");
+  if (glGetShaderInfoLog == NH_NULL) return false;
+  glDeleteShader = (PFNGLDELETESHADERPROC) SDL_GL_GetProcAddress("glDeleteShader");
+  if (glDeleteShader == NH_NULL) return false;
+
+  glCreateProgram = (PFNGLCREATEPROGRAMPROC) SDL_GL_GetProcAddress("glCreateProgram");
+  if (glCreateProgram == NH_NULL) return false;
+  glAttachShader = (PFNGLATTACHSHADERPROC) SDL_GL_GetProcAddress("glAttachShader");
+  if (glAttachShader == NH_NULL) return false;
+  glLinkProgram = (PFNGLLINKPROGRAMPROC) SDL_GL_GetProcAddress("glLinkProgram");
+  if (glLinkProgram == NH_NULL) return false;
+  glGetProgramiv = (PFNGLGETPROGRAMIVPROC) SDL_GL_GetProcAddress("glGetProgramiv");
+  if (glGetProgramiv == NH_NULL) return false;
+  glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC) SDL_GL_GetProcAddress("glGetProgramInfoLog");
+  if (glGetProgramInfoLog == NH_NULL) return false;
+  glUseProgram = (PFNGLUSEPROGRAMPROC) SDL_GL_GetProcAddress("glUseProgram");
+  if (glUseProgram == NH_NULL) return false;
+  glDeleteProgram = (PFNGLDELETEPROGRAMPROC) SDL_GL_GetProcAddress("glDeleteProgram");
+  if (glDeleteProgram == NH_NULL) return false;
+
+  glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC) SDL_GL_GetProcAddress("glGetUniformLocation");
+  if (glGetUniformLocation == NH_NULL) return false;
+  glUniform1fv = (PFNGLUNIFORM1FVPROC) SDL_GL_GetProcAddress("glUniform1fv");
+  if (glUniform1fv == NH_NULL) return false;
+  glUniform2fv = (PFNGLUNIFORM2FVPROC) SDL_GL_GetProcAddress("glUniform2fv");
+  if (glUniform2fv == NH_NULL) return false;
+  glUniform3fv = (PFNGLUNIFORM3FVPROC) SDL_GL_GetProcAddress("glUniform3fv");
+  if (glUniform3fv == NH_NULL) return false;
+  glUniform4fv = (PFNGLUNIFORM4FVPROC) SDL_GL_GetProcAddress("glUniform4fv");
+  if (glUniform4fv == NH_NULL) return false;
+  glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC) SDL_GL_GetProcAddress("glUniformMatrix4fv");
+  if (glUniformMatrix4fv == NH_NULL) return false;
+  glUniform1f = (PFNGLUNIFORM1FPROC) SDL_GL_GetProcAddress("glUniform1f");
+  if (glUniform1f == NH_NULL) return false;
+  glUniform1ui = (PFNGLUNIFORM1UIPROC) SDL_GL_GetProcAddress("glUniform1ui");
+  if (glUniform1ui == NH_NULL) return false;
+
+  glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC) SDL_GL_GetProcAddress("glEnableVertexAttribArray");
+  if (glEnableVertexAttribArray == NH_NULL) return false;
+  glDisableVertexAttribArray = (PFNGLDISABLEVERTEXATTRIBARRAYPROC) SDL_GL_GetProcAddress("glDisableVertexAttribArray");
+  if (glDisableVertexAttribArray == NH_NULL) return false;
+  glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC) SDL_GL_GetProcAddress("glVertexAttribPointer");
+  if (glVertexAttribPointer == NH_NULL) return false;
+  glGenVertexArrays = (PFNGLGENVERTEXARRAYSPROC) SDL_GL_GetProcAddress("glGenVertexArrays");
+  if (glGenVertexArrays == NH_NULL) return false;
+  glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC) SDL_GL_GetProcAddress("glBindVertexArray");
+  if (glBindVertexArray == NH_NULL) return false;
+  glDeleteVertexArrays = (PFNGLDELETEVERTEXARRAYSPROC) SDL_GL_GetProcAddress("glDeleteVertexArrays");
+  if (glDeleteVertexArrays == NH_NULL) return false;
+
+  glBindImageTexture = (PFNGLBINDIMAGETEXTUREPROC) SDL_GL_GetProcAddress("glBindImageTexture");
+  if (glBindImageTexture == NH_NULL) return false;
+
+  glDispatchCompute = (PFNGLDISPATCHCOMPUTEPROC) SDL_GL_GetProcAddress("glDispatchCompute");
+  if (glDispatchCompute == NH_NULL) return false;
+  glMemoryBarrier = (PFNGLMEMORYBARRIERPROC) SDL_GL_GetProcAddress("glMemoryBarrier");
+  if (glMemoryBarrier == NH_NULL) return false;
+
+  return true;
+}
+
+#endif /* LOADGL_H */
---
+++ src/main.c
@@ -0,0 +1,311 @@
+/* Neushoorn includes */
+#include <nh_base.h>
+#include <ext/nh_logging.h>
+
+/* SDL includes */
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+/* stdlib includes */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Project headers */
+#include "loadgl.h"
+
+/* Globals */
+struct {
+  /* Window */
+  SDL_Window *window;     /* Window handle */
+  SDL_GLContext context;  /* OpenGL context handle */
+  bool running;           /* Is the window running? */
+  i32 width, height;      /* Window dimensions */
+  /* OpenGL */
+  u32 vbo;                /* Vertex buffer object */
+  u32 vao;                /* Vertex array object */
+  u32 shader_program;     /* Shader program */
+  u32 texture;            /* Texture */
+  u32 compute_shader;     /* Compute shader */
+  /* Other state */
+  f32 focal_length;
+} state;
+
+/* Consts */
+const f32 vertices[] = {
+  -1.0f, -1.0f, 0.0f,   0.0f, 0.0f,
+   1.0f, -1.0f, 0.0f,   1.0f, 0.0f,
+   1.0f,  1.0f, 0.0f,   1.0f, 1.0f,
+   1.0f,  1.0f, 0.0f,   1.0f, 1.0f,
+  -1.0f,  1.0f, 0.0f,   0.0f, 1.0f,
+  -1.0f, -1.0f, 0.0f,   0.0f, 0.0f,
+};
+
+/* Utils */
+char *get_file_contents(const char *filename) {
+  FILE *file = fopen(filename, "rb");
+  if (file == NH_NULL) {
+    return NH_NULL;
+  }
+  fseek(file, 0, SEEK_END);
+  size_t filesize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char *contents = (char *)malloc(filesize + 1);
+  fread(contents, 1, filesize, file);
+  fclose(file);
+  contents[filesize] = '\0';
+  return contents;
+}
+
+/* Entry point */
+int main(void) {
+  /* Init SDL */
+  NH_INFO("Initializing SDL...");
+  NH_ASSERT_MSG(SDL_Init(SDL_INIT_VIDEO) == 0, "Failed to initialize SDL");
+  /* Create window */
+  NH_INFO("Creating window...");
+  state.window = SDL_CreateWindow(
+    "OpenGL Compute Shaders",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    640,
+    480,
+    SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+  );
+  NH_ASSERT_MSG(state.window != NH_NULL, "Failed to create window");
+  state.width = 640;
+  state.height = 480;
+  /* Enable OpenGL */
+  NH_INFO("Enabling OpenGL...");
+  SDL_GL_MakeCurrent(state.window, state.context);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+  SDL_GL_LoadLibrary(NULL);
+  /* Create OpenGL context */
+  NH_INFO("Creating OpenGL context...");
+  state.context = SDL_GL_CreateContext(state.window);
+  NH_ASSERT_MSG(state.context != NH_NULL, "Failed to create OpenGL context");
+  NH_LOG_ENTRY("Version: %s", glGetString(GL_VERSION));
+  NH_LOG_ENTRY("Vendor: %s", glGetString(GL_VENDOR));
+  NH_LOG_ENTRY("Renderer: %s", glGetString(GL_RENDERER));
+  /* Enable VSync */
+  NH_INFO("Enabling VSync...");
+  SDL_GL_SetSwapInterval(1);
+
+  /* Create viewport */
+  NH_INFO("Creating viewport...");
+  glViewport(0, 0, state.width, state.height);
+
+  /* Set clear color */
+  NH_INFO("Setting clear color...");
+  NH_LOG_ENTRY("Clear color set to %f, %f, %f, %f", 0.0f, 1.0f, 0.8f, 1.0f);
+  glClearColor(0.0f, 1.0f, 0.8f, 1.0f);
+
+  /* Load OpenGL functions */
+  NH_INFO("Loading OpenGL functions...");
+  NH_ASSERT_MSG(loadGL(), "Failed to load OpenGL functions");
+
+  /* Create vertex array object */
+  NH_INFO("Creating vertex array object...");
+  glGenVertexArrays(1, &state.vao);
+  glBindVertexArray(state.vao);
+
+  /* Create vertex buffer object */
+  NH_INFO("Creating vertex buffer object...");
+  glGenBuffers(1, &state.vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, state.vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  /* Link vertex attributes */
+  NH_INFO("Linking vertex attributes...");
+  /* Position */
+  glVertexAttribPointer(
+      0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (void *)0);
+  glEnableVertexAttribArray(0);
+  /* Texture coordinates */
+  glVertexAttribPointer(
+      1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (void *)(3 * sizeof(f32)));
+  glEnableVertexAttribArray(1);
+
+  /* Unbind vertex array object */
+  glBindVertexArray(0);
+
+  /* Create shader program */
+  NH_INFO("Creating shader program...");
+  int success;
+  char info_log[512];
+  /* Create vertex shader */
+  NH_LOG_ENTRY("Creating vertex shader...");
+  u32 vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  /* Read from shader.vertex */
+  char *vert_shader_source = get_file_contents("shader.vertex");
+  glShaderSource(vertex_shader, 1, (const char**)&vert_shader_source, NULL);
+  free(vert_shader_source);
+  glCompileShader(vertex_shader);
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+    NH_ERROR("Failed to compile vertex shader: %s", info_log);
+  }
+  /* Create fragment shader */
+  NH_LOG_ENTRY("Creating fragment shader...");
+  u32 fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  /* Read from shader.fragment */
+  char *frag_shader_source = get_file_contents("shader.fragment");
+  glShaderSource(fragment_shader, 1, (const char**)&frag_shader_source, NULL);
+  free(frag_shader_source);
+  glCompileShader(fragment_shader);
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+    NH_ERROR("Failed to compile fragment shader: %s", info_log);
+  }
+
+  /* Create shader program */
+  NH_LOG_ENTRY("Creating shader program...");
+  state.shader_program = glCreateProgram();
+  glAttachShader(state.shader_program, vertex_shader);
+  glAttachShader(state.shader_program, fragment_shader);
+  glLinkProgram(state.shader_program);
+  glGetProgramiv(state.shader_program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(state.shader_program, 512, NULL, info_log);
+    NH_ERROR("Failed to link shader program: %s", info_log);
+  }
+  /* Cleanup */
+  NH_LOG_ENTRY("Cleaning up...");
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+  glUseProgram(0);
+
+  /* Create compute shader */
+  NH_INFO("Creating compute shader...");
+  NH_LOG_ENTRY("Creating compute shader...");
+  u32 compute_shader = glCreateShader(GL_COMPUTE_SHADER);
+
+  /* Read from shader.compute */
+  char *compute_shader_source = get_file_contents("shader.compute");
+  glShaderSource(compute_shader, 1, (const char**)&compute_shader_source, NULL);
+  free(compute_shader_source);
+
+  glCompileShader(compute_shader);
+  glGetShaderiv(compute_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(compute_shader, 512, NULL, info_log);
+    NH_ERROR("Failed to compile compute shader: %s", info_log);
+  }
+  /* Create compute program */
+  NH_LOG_ENTRY("Creating compute program...");
+  state.compute_shader = glCreateProgram();
+  glAttachShader(state.compute_shader, compute_shader);
+  glLinkProgram(state.compute_shader);
+  glGetProgramiv(state.compute_shader, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(state.compute_shader, 512, NULL, info_log);
+    NH_ERROR("Failed to link compute program: %s", info_log);
+  }
+  /* Cleanup */
+  NH_LOG_ENTRY("Cleaning up...");
+  glDeleteShader(state.compute_shader);
+  glUseProgram(0);
+
+  /* Create texture */
+  NH_INFO("Creating texture...");
+  glGenTextures(1, &state.texture);
+  glBindTexture(GL_TEXTURE_2D, state.texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  /*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);*/
+  /*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);*/
+  glTexImage2D(
+      GL_TEXTURE_2D,
+      0,
+      GL_RGBA32F,
+      1024, /* Width */
+      1024, /* Height */
+      0,
+      GL_RGBA,
+      GL_UNSIGNED_BYTE,
+      NULL
+  );
+  glBindImageTexture(0, state.texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  /* Main loop */
+  state.running = true;
+  state.focal_length = 1.0f;
+  while (state.running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        state.running = false;
+      }
+      if (event.type == SDL_WINDOWEVENT) {
+        if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+          /* Log window size */
+          NH_INFO(
+              "Window resized to %dx%d",
+              event.window.data1,
+              event.window.data2
+          );
+          /* Update viewport */
+          NH_INFO("Updating viewport...");
+          glViewport(0, 0, event.window.data1, event.window.data2);
+          /* Update state */
+          state.width = event.window.data1;
+          state.height = event.window.data2;
+        }
+      }
+    }
+
+    /* Check for keydown */
+    if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_UP]) {
+      if (state.focal_length < 2.5f)
+        state.focal_length += 0.01f;
+    }
+    if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_DOWN]) {
+      if (state.focal_length > 0.2f)
+        state.focal_length -= 0.01f;
+    }
+
+    /* Clear screen */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Render */
+    glUseProgram(state.shader_program);
+    glBindVertexArray(state.vao);
+    glBindTexture(GL_TEXTURE_2D, state.texture);
+
+    glUseProgram(state.compute_shader);
+    glUniform1f(glGetUniformLocation(state.compute_shader, "width"), (f32)state.width);
+    glUniform1f(glGetUniformLocation(state.compute_shader, "height"), (f32)state.height);
+    glUniform1f(glGetUniformLocation(state.compute_shader, "focal_length"), state.focal_length);
+    glUniform1ui(glGetUniformLocation(state.compute_shader, "random_seed"), rand());
+    glDispatchCompute(1024 / 32, 1024 / 32, 1);
+    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+    glUseProgram(state.shader_program);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindVertexArray(0);
+
+    /* Swap buffers */
+    SDL_GL_SwapWindow(state.window);
+  }
+
+  /* Clean up */
+  NH_INFO("Cleaning up...");
+  glDeleteTextures(1, &state.texture);
+  glDeleteProgram(state.compute_shader);
+  glDeleteProgram(state.shader_program);
+  glDeleteBuffers(1, &state.vbo);
+  glDeleteVertexArrays(1, &state.vao);
+  SDL_GL_DeleteContext(state.context);
+  SDL_DestroyWindow(state.window);
+  SDL_Quit();
+  return 0;
+}
