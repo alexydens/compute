150bec0a2ddeb2c3ba80a7301b859a49e142ee95
alexydens <alexydens@outlook.com>
2024-05-18 17:11:38 +0100

    I have no idea when I last did a commit. There have been some changes...

--- src/main.c
+++ src/main.c
@@ -15,13 +15,23 @@
 /* Project headers */
 #include "loadgl.h"
 
+/* Structs */
+typedef struct {
+  nh_vec2_t pos;
+  f32 min, max;
+  const char *text;
+  f32 *value;
+  bool active;
+  u8 for_active; /* Is SDL_SCANCODE */
+  f32 sensitivity;
+} slider_t;
+
 /* Consts */
 #define COMPUTE_WIDTH       512
 #define COMPUTE_HEIGHT      512
 #define FONT_COLS           24
 #define FONT_ROWS           4
 
-/* Consts */
 const char *font_chars =  " !\"#$%&'()*+,-./01234567"
                           "89:;<=>?@ABCDEFGHIJKLMNO"
                           "PQRSTUVWXYZ[\\]^_`abcdefg"
@@ -50,15 +60,25 @@ struct {
   u32 texture;                  /* Texture */
   u32 font_texture;             /* Font texture */
   u32 compute_shader;           /* Compute shader */
+  u32 solid_shader;             /* Solid shader */
   /* Other state */
   f32 focal_length;             /* Focal length */
   u32 ticks;                    /* Ticks since last movement */
   f32 delta_time;               /* Delta time */
   f32 fps;                      /* Frames per second */
+  char fps_string[64];          /* FPS string */
+  char delta_string[64];        /* Delta time string */
   f32 test_in;                  /* An input used for testing */
   f32 angle_x, angle_y;         /* Camera rotation: yaw, pitch */
   nh_vec3_t camera;             /* Camera position */
+  u8 active_slider;             /* Active slider */
 } state;
+
+/* More state */
+slider_t sliders[] = {
+  (slider_t){(nh_vec2_t){-0.925f, 0.925f}, 0.0f, 10.0f, "[T] Test Input:   ", &state.test_in, false, SDL_SCANCODE_T, 20.0f},
+  (slider_t){(nh_vec2_t){-0.925f, 0.875f}, 0.0f, 5.0f,  "[F] Focal Length: ", &state.focal_length, false, SDL_SCANCODE_F, 5.0f},
+};
 
 /* Utils */
 char *get_file_contents(const char *filename) {
@@ -127,10 +147,58 @@ void render_character(char c, nh_vec2_t pos, f32 scale) {
   glDeleteBuffers(1, &char_vbo);
   glBindTexture(GL_TEXTURE_2D, 0);
 }
-void render_string(const char *str, nh_vec2_t pos, f32 scale) {
+f32 render_string(const char *str, nh_vec2_t pos, f32 scale) {
+  f32 size = 0;
   for (u32 i = 0; str[i] != '\0'; i++) {
     render_character(str[i], (nh_vec2_t){pos.x+(i+1)*scale, pos.y}, scale);
+    size += scale;
   }
+  return size;
+}
+void render_slider(slider_t slider) {
+  const f32 start_x = render_string(slider.text, slider.pos, 0.025f) + 0.025f;
+  const f32 max_length = 0.5f;
+  const f32 slider_x = start_x + max_length * (*(slider.value) - slider.min) / (slider.max - slider.min);
+  /*render_string("=", (nh_vec2_t){slider.pos.x+slider_x, slider.pos.y}, 0.025f);*/
+  render_string("]", (nh_vec2_t){slider.pos.x+start_x+max_length, slider.pos.y}, 0.025f);
+  render_string("[", (nh_vec2_t){slider.pos.x+start_x-0.025f, slider.pos.y}, 0.025f);
+  /* Create slider vao */
+  u32 slider_vao, slider_vbo;
+  glGenVertexArrays(1, &slider_vao);
+  glBindVertexArray(slider_vao);
+  /* Create slider_vertices - position, texture coords */
+  f32 slider_vertices[] = {
+    slider.pos.x + start_x,  slider.pos.y-0.025f,  0.5f,   0.0f, 0.0f,
+    slider.pos.x + slider_x, slider.pos.y-0.025f,  0.5f,   1.0f, 0.0f,
+    slider.pos.x + slider_x, slider.pos.y+0.025f,  0.5f,   1.0f, 1.0f,
+    slider.pos.x + start_x,  slider.pos.y-0.025f,  0.5f,   0.0f, 0.0f,
+    slider.pos.x + slider_x, slider.pos.y+0.025f,  0.5f,   1.0f, 1.0f,
+    slider.pos.x + start_x,  slider.pos.y+0.025f,  0.5f,   0.0f, 1.0f
+  };
+  /* Create slider vbo */
+  glGenBuffers(1, &slider_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, slider_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(f32) * 6 * 5, slider_vertices, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (void *)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (void *)(3 * sizeof(f32)));
+  /* Render slider */
+  glUseProgram(state.solid_shader);
+  if (slider.active) {
+    nh_vec3_t color = (nh_vec3_t){1.0f, 1.0f, 1.0f};
+    glUniform3fv(glGetUniformLocation(state.solid_shader, "color"), 1, (f32 *)&color);
+  } else {
+    nh_vec3_t color = (nh_vec3_t){0.4f, 0.4f, 0.4f};
+    glUniform3fv(glGetUniformLocation(state.solid_shader, "color"), 1, (f32 *)&color);
+  }
+  glBindVertexArray(slider_vao);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glDeleteVertexArrays(1, &slider_vao);
+  glDeleteBuffers(1, &slider_vbo);
+  glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 /* Entry point */
@@ -142,7 +210,7 @@ int main(void) {
   /* Create window */
   NH_INFO("Creating window...");
   state.window = SDL_CreateWindow(
-    "OpenGL Compute Shaders",
+    "OpenGL Compute Shader Path Tracing Demo",
     SDL_WINDOWPOS_UNDEFINED,
     SDL_WINDOWPOS_UNDEFINED,
     640,
@@ -175,7 +243,8 @@ int main(void) {
   /* Set clear color */
   NH_INFO("Setting clear color...");
   NH_LOG_ENTRY("Clear color set to %f, %f, %f, %f", 0.0f, 1.0f, 0.8f, 1.0f);
-  glClearColor(0.0f, 1.0f, 0.8f, 1.0f);
+  /*glClearColor(0.0f, 1.0f, 0.8f, 1.0f);*/
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -287,6 +356,53 @@ int main(void) {
   glDeleteShader(state.compute_shader);
   glUseProgram(0);
 
+  /* Create shader program */
+  NH_INFO("Creating solid shader program...");
+  /* Create vertex shader */
+  NH_LOG_ENTRY("Creating vertex shader...");
+  u32 solid_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  /* Read from shader.vertex */
+  char *solid_vert_shader_source = get_file_contents("shader.vertex");
+  glShaderSource(solid_vertex_shader, 1, (const char**)&solid_vert_shader_source, NULL);
+  free(solid_vert_shader_source);
+  glCompileShader(solid_vertex_shader);
+  glGetShaderiv(solid_vertex_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(solid_vertex_shader, 512, NULL, info_log);
+    NH_ERROR("Failed to compile vertex shader: %s", info_log);
+  }
+  /* Create fragment shader */
+  NH_LOG_ENTRY("Creating fragment shader...");
+  u32 solid_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  /* Read from solid.fragment */
+  char *solid_frag_shader_source = get_file_contents("solid.fragment");
+  glShaderSource(solid_fragment_shader, 1, (const char**)&solid_frag_shader_source, NULL);
+  free(solid_frag_shader_source);
+  glCompileShader(solid_fragment_shader);
+  glGetShaderiv(solid_fragment_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(solid_fragment_shader, 512, NULL, info_log);
+    NH_ERROR("Failed to compile fragment shader: %s", info_log);
+  }
+
+  /* Create shader program */
+  NH_LOG_ENTRY("Creating shader program...");
+  state.solid_shader = glCreateProgram();
+  glAttachShader(state.solid_shader, solid_vertex_shader);
+  glAttachShader(state.solid_shader, solid_fragment_shader);
+  glLinkProgram(state.solid_shader);
+  glGetProgramiv(state.solid_shader, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(state.solid_shader, 512, NULL, info_log);
+    NH_ERROR("Failed to link shader program: %s", info_log);
+  }
+  /* Cleanup */
+  NH_LOG_ENTRY("Cleaning up...");
+  glDeleteShader(solid_vertex_shader);
+  glDeleteShader(solid_fragment_shader);
+  glUseProgram(0);
+
+
   /* Create texture */
   NH_INFO("Creating main texture...");
   glGenTextures(1, &state.texture);
@@ -341,6 +457,8 @@ int main(void) {
   state.angle_y = -0.3f;
   state.camera = (nh_vec3_t){0.0f, 1.5f, 0.0f};
   state.keys = SDL_GetKeyboardState(NULL);
+  state.active_slider = 0;
+  sliders[0].active = true;
   while (state.running) {
     /* Delta time - 1 */
     u64 start = SDL_GetPerformanceCounter();
@@ -348,25 +466,27 @@ int main(void) {
     /* Event loop */
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-        state.running = false;
-      }
-      if (event.type == SDL_WINDOWEVENT) {
-        if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
-          /* Log window size */
-          NH_INFO(
-              "Window resized to %dx%d",
-              event.window.data1,
-              event.window.data2
-          );
-          /* Update viewport */
-          glViewport(0, 0, event.window.data1, event.window.data2);
-          /* Update state */
-          state.width = event.window.data1;
-          state.height = event.window.data2;
-          /* Reset ticks */
-          state.ticks = 0;
-        }
+      switch (event.type) {
+        case (SDL_QUIT): {
+          state.running = false;
+        } break;
+        case (SDL_WINDOWEVENT): {
+          if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+            /* Log window size */
+            NH_INFO(
+                "Window resized to %dx%d",
+                event.window.data1,
+                event.window.data2
+            );
+            /* Update viewport */
+            glViewport(0, 0, event.window.data1, event.window.data2);
+            /* Update state */
+            state.width = event.window.data1;
+            state.height = event.window.data2;
+            /* Reset ticks */
+            state.ticks = 0;
+          }
+        } break;
       }
     }
 
@@ -434,16 +554,37 @@ int main(void) {
       state.ticks = 0;
     }
 
+#if 0
     /* Plus = increase test in */
     if (state.keys[SDL_SCANCODE_KP_PLUS]) {
       state.test_in += 20.0f * state.delta_time;
+      if (state.test_in > 5.0f) state.test_in = 5.0f;
       state.ticks = 0;
     }
     /* Minus = decrease test in */
     if (state.keys[SDL_SCANCODE_KP_MINUS]) {
       state.test_in -= 20.0f * state.delta_time;
+      if (state.test_in < 0.0f) state.test_in = 0.0f;
       state.ticks = 0;
     }
+#endif
+    /* Plus = increase active slider */
+    if (state.keys[SDL_SCANCODE_KP_PLUS]) {
+      *(sliders[state.active_slider].value) += sliders[state.active_slider].sensitivity * state.delta_time;
+      if (*(sliders[state.active_slider].value) > sliders[state.active_slider].max) {
+        *(sliders[state.active_slider].value) = sliders[state.active_slider].max;
+      }
+      state.ticks = 0;
+    }
+    /* Minus = decrease active slider */
+    if (state.keys[SDL_SCANCODE_KP_MINUS]) {
+      *(sliders[state.active_slider].value) -= sliders[state.active_slider].sensitivity * state.delta_time;
+      if (*(sliders[state.active_slider].value) < sliders[state.active_slider].min) {
+        *(sliders[state.active_slider].value) = sliders[state.active_slider].min;
+      }
+      state.ticks = 0;
+    }
+
     /* M+W = wireframe mode */
     if (state.keys[SDL_SCANCODE_M]
         && state.keys[SDL_SCANCODE_W]) {
@@ -453,6 +594,15 @@ int main(void) {
     if (state.keys[SDL_SCANCODE_M]
         && state.keys[SDL_SCANCODE_R]) {
       glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+
+    /* Sliders */
+    for (u32 i = 0; i < sizeof(sliders) / sizeof(sliders[0]); i++) {
+      if (state.keys[sliders[i].for_active]) {
+        sliders[state.active_slider].active = false;
+        state.active_slider = i;
+        sliders[i].active = true;
+      }
     }
 
     /* Clear screen */
@@ -490,9 +640,18 @@ int main(void) {
     glBindTexture(GL_TEXTURE_2D, 0);
     glBindVertexArray(0);
 
-    /* Render string */
-    //render_string("Hello world!", (nh_vec2_t){-1.0f, 0.975f}, 0.025f);
-    render_string("Hello world!", (nh_vec2_t){-1.0f, 0.975f}, 0.025f);
+    /* Render hints */
+    render_string("[M][W]       = wireframe", (nh_vec2_t){-0.925f, -0.925f}, 0.025f);
+    render_string("[M][R]       = regular", (nh_vec2_t){-0.925f, -0.875f}, 0.025f);
+    render_string("[W][A][S][D] = move", (nh_vec2_t){-0.925f, -0.825f}, 0.025f);
+    render_string("<ARROW-KEYS> = look", (nh_vec2_t){-0.925f, -0.775f}, 0.025f);
+    /* Render sliders */
+    for (u32 i = 0; i < sizeof(sliders) / sizeof(sliders[0]); i++) {
+      render_slider(sliders[i]);
+    }
+    /* Draw delta time and FPS to the screen */
+    render_string(state.delta_string, (nh_vec2_t){0.5f, 0.925f}, 0.025f);
+    render_string(state.fps_string, (nh_vec2_t){0.5f, 0.875f}, 0.025f);
 
     /* Swap buffers */
     SDL_GL_SwapWindow(state.window);
@@ -505,8 +664,11 @@ int main(void) {
     f64 delta = (f64)(end - start) / (f64)SDL_GetPerformanceFrequency();
     state.delta_time = delta;
     state.fps = 1.0f / delta;
-    if (state.ticks % 80 == 0)
-      NH_INFO("Delta time: %.2fms, FPS: %f", delta * 1000.0, state.fps);
+    if (state.ticks % 60 == 0) {
+      /*NH_INFO("Delta time: %.2fms, FPS: %f", delta * 1000.0, state.fps);*/
+      sprintf(state.fps_string, "FPS: %f", state.fps);
+      sprintf(state.delta_string, "Delta time: %.2fms", delta * 1000.0);
+    }
   }
 
   /* Clean up */
